d380c538c27532b25ced53d9537db6e7a855d975
Soumya ranjan mishra <soumyamishra637@gmail.com>
2024-06-03 18:13:34 +0530

    Struct Vs Class

---
+++ KVCKVO/KVCKVO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KVCKVO
+//
+//  Created by Ashis Kumar Mishra on 30/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StructVsClass/StructVsClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StructVsClass
+//
+//  Created by Ashis Kumar Mishra on 03/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
